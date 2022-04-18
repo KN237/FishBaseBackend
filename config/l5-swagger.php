b8dc52e277285a0953e8dec3d637febc47b31649
KN237 <kanamboma@gmail.com>
2022-04-18 18:25:07 +0200

    URL debug 2

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -281,7 +281,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost/api'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST'),
         ],
     ],
 ];
